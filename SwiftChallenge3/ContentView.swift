75ddca6db900c578f26db32bca07eeb4dd7647ce
kakutagawa <kaktgw0824@gmail.com>
2024-01-03 16:06:48 +0900

    Initial Commit

---
+++ SwiftChallenge3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftChallenge3
+//
+//  Created by 芥川浩平 on 2024/01/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftChallenge3/SwiftChallenge3App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftChallenge3App.swift
+//  SwiftChallenge3
+//
+//  Created by 芥川浩平 on 2024/01/03.
+//
+
+import SwiftUI
+
+@main
+struct SwiftChallenge3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
